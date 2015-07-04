eff133026f0436e94d061fca870b4cf6750ce538
Jorge Iván Andrés Contreras Pereira <jorge@contreras.com.co>
2015-07-03 17:58:57 -1200

    100% coverage

--- src/co/edu/unal/MyFilesTest.java
+++ src/co/edu/unal/MyFilesTest.java
@@ -21,6 +21,32 @@ import org.junit.Test;
 public class MyFilesTest {
 
 	/**
+	 * Test method for {@link co.edu.unal.MyFiles#getPath(java.lang.String)}.
+	 * @throws IOException 
+	 */
+	@Test
+	public void testGetPath() throws IOException {
+		File file1 = new File("./goku.txt");
+		file1.createNewFile();
+		//Create initial file
+		MyFiles file = new MyFiles("./","Amici di Goku",file1);
+		assertTrue("Compara", file.getPath().equals("./"));		
+	}
+	
+	/**
+	 * Test method for {@link co.edu.unal.MyFiles#getFile(File)}.
+	 * @throws IOException 
+	 */
+	@Test
+	public void testGetFile() throws IOException {
+		File file1 = new File("./goku.txt");
+		file1.createNewFile();
+		//Create initial file
+		MyFiles file = new MyFiles("./","Amici di Goku",file1);
+		assertTrue("Compara", file.getFile().equals(file1));		
+	}
+	
+	/**
 	 * Test method for {@link co.edu.unal.MyFiles#addContent(java.lang.String)}.
 	 * @throws IOException 
 	 */
